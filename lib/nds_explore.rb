aa22805fac527bc67ead07ff727b890189f66951
William Lin <willlin1996@gmail.com>
2020-02-28 17:49:53 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -15,7 +15,7 @@ def print_first_directors_movie_titles
 
   spielberg_movies = directors_database[0][:movies]
   while index < spielberg_movies.length do
-     return spielberg_movies[index][:title]
+     puts spielberg_movies[index][:title]
      index += 1
   end
 end
