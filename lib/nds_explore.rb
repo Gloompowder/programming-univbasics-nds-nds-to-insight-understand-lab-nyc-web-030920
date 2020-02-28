0bef8ab30a6607e525a80698c24906c0c4c1bd92
William Lin <willlin1996@gmail.com>
2020-02-28 17:47:39 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -15,7 +15,7 @@ def print_first_directors_movie_titles
   binding.pry
   spielberg_movies = directors_database[0][:movies]
   while index < spielberg_movies.length do
-    
+     spielberg_movies[index][:title]
   end
   index += 1
 end
